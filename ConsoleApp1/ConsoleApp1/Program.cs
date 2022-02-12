730a84c98df17c5666506294163c0563764f3058
volodymyr.matseliukh <volodymyr.matselyukh@yandex.ru>
2022-02-12 15:58:31 +0200

    implementing recursion

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,6 +17,8 @@ namespace ConsoleApp1
          *  2. INTEGER p
          */
 
+        public const int Modulo = 1000000007;
+
         public static long pSequences(int n, int p)
         {
             const int Modulo = 1000000007;
@@ -47,7 +49,7 @@ namespace ConsoleApp1
 
             baseDict = new Dictionary<int, int>(sortedBaseDict);
 
-            var baseDictKeys = baseDict.Keys.ToList();
+            var baseDictKeys = baseDict.Keys.ToList();//baseDict.Keys.ToHashSet();
             var baseDictKeysCount = baseDict.Count;
 
             for (int i = 0; i < baseDictKeysCount; i++)
@@ -89,7 +91,7 @@ namespace ConsoleApp1
                         }
                         else
                         {
-                            previousValue += diffDict[j] * prevDict[baseDict[baseDictKeys[j]]];
+                            previousValue += diffDict[j] * prevDict[baseDictKeys[baseDictKeysCount - j - 1]];
                             nextDict[baseDictKeys[j]] = previousValue > Modulo ? previousValue % Modulo : previousValue;
                         }
                     }
@@ -99,6 +101,86 @@ namespace ConsoleApp1
             }
 
             return nextDict[p];
+        }
+
+        public static long pSequencesV2(int n, int p)
+        {
+            SortedDictionary<int, int> sortedBaseDict = new SortedDictionary<int, int>();
+            Dictionary<int, int> baseDict = new Dictionary<int, int>();
+            Dictionary<int, int> diffDict = new Dictionary<int, int>();
+            Dictionary<string, long> acumulativeDict = new Dictionary<string, long>();
+            Dictionary<int, long> prevDict = new Dictionary<int, long>();
+            Dictionary<int, long> nextDict = new Dictionary<int, long>();
+
+
+            int squareRoot = (int)Math.Sqrt(p);
+
+            for (int i = 1; i <= squareRoot; i++)
+            {
+                int division = p / i;
+
+                sortedBaseDict[i] = division;
+
+                if (!sortedBaseDict.ContainsKey(division))
+                {
+                    sortedBaseDict[division] = i;
+                }
+            }
+
+            baseDict = new Dictionary<int, int>(sortedBaseDict);
+
+            var baseDictKeys = baseDict.Keys.ToList();
+            var baseDictKeysCount = baseDict.Count;
+
+            for (int i = 0; i < baseDictKeysCount; i++)
+            {
+                var currentKey = baseDictKeys[i];
+                var previousKey = i == 0 ? 0 : baseDictKeys[i - 1];
+
+                diffDict[currentKey] = currentKey - previousKey;
+            }
+
+            return recursion(acumulativeDict, diffDict, baseDict, baseDictKeys, baseDictKeysCount, n + 1, 1);
+        }
+
+        public static long recursion(Dictionary<string, long> acumulativeDict, Dictionary<int, int> diffDict, Dictionary<int, int> baseDict, List<int> baseDictKeys, int baseDictKeysCount, int n, int p)
+        {
+            int currentKey = p;
+            var key = $"{n},{currentKey}";
+            if (acumulativeDict.ContainsKey(key))
+            {
+                return acumulativeDict[key];
+            }
+            else if (n == 2)
+            {
+                return baseDict[p];
+            }
+
+
+            long sum = 0;            
+
+            int index = 0;
+
+            currentKey = baseDictKeys[index];
+            do
+            {
+                long recursionResult = diffDict[currentKey] * recursion(acumulativeDict, diffDict, baseDict, baseDictKeys, baseDictKeysCount, n - 1, currentKey);
+                        
+                if (recursionResult > Modulo)
+                {
+                    recursionResult %= Modulo;
+                }
+                    
+                sum += recursionResult;
+
+                index++;
+            }
+            while (index < baseDictKeysCount && (currentKey = baseDictKeys[index]) <= baseDict[p]);
+
+            key = $"{n},{p}";
+            acumulativeDict[key] = sum;
+
+            return sum;
         }
 
         public static void printNumber(List<int> nList, int n)
@@ -124,10 +206,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            long result = Result.pSequences(899, 1178162386);
+            //long result = Result.pSequences(899, 1178162386);
             //long result = Result.pSequences(899, 78162386);
             //long result = Result.pSequences(3, 4);
-            //long result = Result.pSequences(3, 10);
+            //long result = Result.pSequences(3, 10); //147
+            //long result = Result.pSequences(4, 10); //544
+
+            //################  v2  ################
+            //long result = Result.pSequences(899, 1178162386);
+            long result = Result.pSequencesV2(899, 78162386);
+            //long result = Result.pSequencesV2(3, 4);
+            //long result = Result.pSequencesV2(3, 10); //147
+            //long result = Result.pSequencesV2(4, 10); //544
+            //long result = Result.pSequencesV2(500, 1000); //544
+
             Console.WriteLine($"result {result}");
         }
     }
